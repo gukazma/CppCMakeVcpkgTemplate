8c88807e64d624c4ca9efe39839a975e6589b528
gukazma <gukazma@proton.me>
2024-06-14 14:20:03 +0800

    🦄 refactor: auto libs

--- Apps/DemoApp/main.cpp
+++ Apps/DemoApp/main.cpp
@@ -1,21 +1,7 @@
-#include "MainWindow.h"
-#include <QApplication>
-#include <QSplashScreen>
-#include <QTranslator>
+#include <iostream>
+
 int main(int argc, char** argv)
 {
-    QApplication  app(argc, argv);
-    QPixmap       pixmap(QString::fromLocal8Bit(":/mgukazma.jpg"));
-    QSplashScreen splash(pixmap);
-    splash.show();
-
-    QTranslator* translator = new QTranslator;
-    translator->load(":/Translations/zh_CN.qm");
-    app.installTranslator(translator);
-
-    MainWindow mainwindow;
-    mainwindow.showMaximized();
-
-    splash.finish(&mainwindow);
-    return app.exec();
+    std::cout << "hello world!" << std::endl;
+    return 0;
 }
